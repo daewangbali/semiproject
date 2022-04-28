d051d0529df8cebd102318c5999ce730542dcd96
daewangbali <daewangbali@gmail.com>
2022-04-28 17:42:40 +0900

    220428/dawoon/helloworld!

---
+++ semiproject/src/main/java/org/kosta/ekoprojecct/controller/HomeController.java
@@ -0,0 +1,14 @@
+package org.kosta.ekoprojecct.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HomeController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	
+		return "home.jsp";
+	}
+
+}
