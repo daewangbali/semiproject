86b0131f307f3456151313d535378afe47aebfb1
Seoin-Jeong <seoin0529@gmail.com>
2022-05-02 14:32:47 +0900

    220502/seoin/create MemberDAO,LoginController

---
+++ semiproject/src/main/java/org/kosta/ekoprojecct/controller/LoginController.java
@@ -0,0 +1,30 @@
+package org.kosta.ekoprojecct.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.kosta.ekoprojecct.model.MemberVO;
+
+public class LoginController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		String id=request.getParameter("id");
+		String password=request.getParameter("password");
+		
+		MemberVO vo=MemberDAO.getInstance().login(id,password);
+		String viewName=null;
+		if(vo==null) {
+			viewName="login-fail.jsp";
+		}else {
+			HttpSession session=request.getSession();
+			session.setAttribute("mvo", vo);
+			viewName="redirect:ListController.do";
+		}
+		
+		return viewName;
+	}
+	
+}
