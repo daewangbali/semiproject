5db0a64fe812256a67fbe9eae5bff0dea0914a4c
Jahyung-Gu <gjh1856@gmail.com>
2022-05-03 16:44:47 +0900

    220503/Jahyung-Gu/create MyPageController

---
+++ semiproject/src/main/java/org/kosta/ekoprojecct/controller/UpdateMemberFormController.java
@@ -0,0 +1,14 @@
+package org.kosta.ekoprojecct.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UpdateMemberFormController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
